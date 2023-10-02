d001de94a683ddfd48147afb3e159469c121db28
unknown <karanveershekhawatna13@gmail.com>
2023-10-02 19:17:10 +0530

    second commit

--- money_splitter/settings.py
+++ money_splitter/settings.py
@@ -11,10 +11,18 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+
+import dj_database_url
+
 from . info import *
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 EMAIL_USE_TLC = EMAIL_USE_TLC
 EMAIL_HOST = EMAIL_HOST
@@ -27,14 +35,17 @@ EMAIL_PORT = EMAIL_PORT
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8livpb-366)0k*6zl)z6sr#49q1k!h%4%j)6)m=cu#5%d2_pnj'
+SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = 'django-insecure-8livpb-366)0k*6zl)z6sr#49q1k!h%4%j)6)m=cu#5%d2_pnj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+# DEBUG = True
 
 TASTYPIE_FULL_DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'expenser-app-django-heroku.herokuapp.com']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'expenser-app-django-heroku.herokuapp.com']
 #ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 # From Google https://expenser-app-django-heroku.herokuapp.com/
@@ -94,12 +105,12 @@ WSGI_APPLICATION = 'money_splitter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
+}
 
 # DATABASES = {
 #    'default': {
@@ -134,16 +145,32 @@ WSGI_APPLICATION = 'money_splitter.wsgi.application'
 #postgres://ucinxdojytcwgj:c927b1a5132524831ace13f01f38f2c02142ca479d79cb0aeafb9ddf97fa6d5c@ec2-54-173-77-184.compute-1.amazonaws.com:5432/d6t8v3jin1pml
 #postgres://yihsatrfvblsab:bc6d6e22baf9e9ca7c67e95fc674e8b6f15e9cae2941fe834fc6d7bd4cef5870@ec2-54-158-247-210.compute-1.amazonaws.com:5432/d2ep16s0ic9h4d
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd2ep16s0ic9h4d',
-        'USER': 'yihsatrfvblsab',
-        'PASSWORD': 'bc6d6e22baf9e9ca7c67e95fc674e8b6f15e9cae2941fe834fc6d7bd4cef5870',
-        'HOST': 'ec2-54-158-247-210.compute-1.amazonaws.com',
-        'PORT': '',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': os.environ['ENGINE'],
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'HOST': os.environ['HOST'],
+#         'NAME': os.environ['NAME'],
+#         'USER': os.environ['USER'],
+#         'PASSWORD': os.environ['PASSWORD'],
+#         'PORT': os.environ['PORT'],
+#     }
+# }
+
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
+
+#Actual on
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'd2ep16s0ic9h4d',
+#         'USER': 'yihsatrfvblsab',
+#         'PASSWORD': 'bc6d6e22baf9e9ca7c67e95fc674e8b6f15e9cae2941fe834fc6d7bd4cef5870',
+#         'HOST': 'ec2-54-158-247-210.compute-1.amazonaws.com',
+#         'PORT': '',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
